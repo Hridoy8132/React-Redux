0964c802b189c2ecec9dc1deb3379c29a782581e
Md. Nurul Haider Hridoy <hridoy0182726@gmail.com>
2024-05-22 18:39:23 +0600

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+// React-Redux start lec-6 Design add to Cart.
+import React from 'react'
+
+function Home(props) {
+    return (
+        <div>
+           
+            <h1>Home Component</h1>
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                    <img src="https://image.telanganatoday.com/wp-content/uploads/2022/03/iphone_V_jpg--816x480-4g.webp?sw=1366&dsz=816x480&iw=659&p=false&r=1" />
+                </div>
+                <div className="text-wrapper item">
+                    <span>
+                        I-Phone
+                    </span>
+                    <span>
+                        Price: $1000.00
+                    </span>
+                </div>
+                <div className="btn-wrapper item">
+                    <button 
+                    onClick={
+                        ()=>{props.addToCartHandler({price:1000,name:'i phone 11'})}
+                        }>
+                        Add To Cart</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home
